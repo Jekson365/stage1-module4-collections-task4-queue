4fc9b34ac0278b0787b09d20b8e34bd9fe9b22bd
jeko <jemali.ergemlidze.1@btu.edu.ge>
2023-08-01 22:51:38 +0400

    xa

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,18 +1,19 @@
 package com.epam.collections.queue;
 
 import java.util.ArrayDeque;
+import java.util.Objects;
 import java.util.PriorityQueue;
 import java.util.Queue;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
-        ArrayDeque<Integer> arr = new ArrayDeque<>();
+        ArrayDeque<Integer> arrayDeque = new ArrayDeque<>();
 
         while (!firstQueue.isEmpty() && !secondQueue.isEmpty()) {
-            arr.add(firstQueue.poll());
-            arr.add(secondQueue.poll());
+            arrayDeque.add(firstQueue.poll());
+            arrayDeque.add(secondQueue.poll());
         }
 
-        return arr;
+        return arrayDeque;
     }
 }
--- src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
+++ src/main/java/com/epam/collections/queue/PriorityQueueCreator.java
@@ -7,13 +7,14 @@ import java.util.PriorityQueue;
 
 public class PriorityQueueCreator {
     public PriorityQueue<String> createPriorityQueue(List<String> firstList, List<String> secondList) {
+        List<String> mergedList = new ArrayList<>(firstList);
+        mergedList.addAll(secondList);
 
-        List<String> m = new ArrayList<>(firstList);
-        m.addAll(secondList);
+        PriorityQueue<String> priorityQueue = new PriorityQueue<>(
+                (s1, s2) -> s2.compareTo(s1)
+        );
 
-        Collections.sort(m, Collections.reverseOrder());
-
-        PriorityQueue<String> priorityQueue = new PriorityQueue<>(m);
+        priorityQueue.addAll(mergedList);
 
         return priorityQueue;
     }
